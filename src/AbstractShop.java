13a58f4c2d4958eddcb581fa630698b70143213e
IlyaPechersky <foodgamesimple@gmail.com>
2022-04-27 15:57:10 +0300

    added database

--- src/AbstractShop.java
+++ src/AbstractShop.java
@@ -1,6 +1,30 @@
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
 public abstract class AbstractShop extends AbstractUser {
+    private Map<String, Integer> assortment;
+
     AbstractShop(String login, String password) {
         super(login, password);
+        assortment = new HashMap<>();
+    }
+
+    public void addProduct(String product) {
+        assortment.put(product, assortment.getOrDefault(product, 0) + 1);
+    }
+
+    public boolean hasProducts() {
+        return !assortment.isEmpty();
+    }
+
+    public void printProducts(PrintWriter writer) {
+        for (String key : assortment.keySet()) {
+            for (int i = 0; i < assortment.get(key); i++) {
+                writer.write(key + " ");
+            }
+        }
+        writer.write("\n");
     }
 
     abstract String getShopType();
--- src/Server.java
+++ src/Server.java
@@ -1,17 +1,83 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.*;
 
 public class Server {
     private static Server instance;
     private static Map<String, String> loginToPasswordDB;
     private static Map<String, AbstractUser> loginToUserDB;
+    private static List<AbstractShop> shopList;
+    private static Scanner scanner;
+    private static PrintWriter writer;
 
-    private Server() {
+    private Server() throws FileNotFoundException {
         loginToPasswordDB = new HashMap<>();
         loginToUserDB = new HashMap<>();
+        shopList = new ArrayList<>();
+        readDataBase();
     }
 
-    public static Server getInstance() {
+    private void readDataBase() throws FileNotFoundException {
+        scanner = new Scanner(new File("database.txt"));
+        while (scanner.hasNext()) {
+            String type = scanner.next();
+            String foodType = "";
+            if (type.equals("FoodShop")) foodType = scanner.next();
+            String login = scanner.next();
+            String password = scanner.next();
+            loginToPasswordDB.put(login, password);
+
+            if (type.equals("User")) {
+                loginToUserDB.put(login, new User(login, password));
+            } else {
+                boolean hasProducts = scanner.next().equals("true");
+
+                if (type.equals("HealthShop")) {
+                    loginToUserDB.put(login, new HealthShop(login, password));
+                } else {
+                    Food food = Food.VEGETABLE;
+                    if (foodType.equals("GROCERY")) food = Food.GROCERY;
+                    if (foodType.equals("FRUIT")) food = Food.FRUIT;
+                    loginToUserDB.put(login, new FoodShop(login, password).setShopType(food));
+                }
+
+
+                AbstractShop shop = (AbstractShop) loginToUserDB.get(login);
+                if (hasProducts) {
+                    scanner.nextLine();
+                    Arrays.stream(scanner.nextLine().split(" ")).forEach(shop::addProduct);
+                }
+
+                shopList.add(shop);
+            }
+        }
+        scanner.close();
+    }
+
+    public void loadDataBase() throws FileNotFoundException {
+        writer = new PrintWriter("database.txt");
+        for (String login : loginToUserDB.keySet()) {
+            if (loginToUserDB.get(login) instanceof User) {
+                writer.write("User " + login + " " + loginToPasswordDB.get(login) + "\n");
+            } else if (loginToUserDB.get(login) instanceof FoodShop) {
+                FoodShop shop = (FoodShop) loginToUserDB.get(login);
+                writer.write("FoodShop " + shop.getShopType() + " " + login + " " + loginToPasswordDB.get(login));
+                if (shop.hasProducts()) writer.write(" true\n");
+                else writer.write(" false\n");
+                shop.printProducts(writer);
+            } else {
+                HealthShop shop = (HealthShop) loginToUserDB.get(login);
+                writer.write("HealthShop " + login + " " + loginToPasswordDB.get(login));
+                if (shop.hasProducts()) writer.write(" true\n");
+                else writer.write(" false\n");
+                shop.printProducts(writer);
+            }
+        }
+        writer.close();
+    }
+
+    public static Server getInstance() throws FileNotFoundException {
         if (instance == null) {
             instance = new Server();
         }
@@ -30,6 +96,11 @@ public class Server {
             return true;
         }
         return false;
+    }
+
+    public void printShopList() {
+        //todo
+
     }
 
     public boolean registerUser(String login, String password, AbstractUser user) {
